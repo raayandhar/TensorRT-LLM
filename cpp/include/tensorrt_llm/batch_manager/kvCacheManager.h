b6a16079bc5860caf5037bba4766b34a84c6cc27
Tomer Shmilovich <tshmilovich@nvidia.com>
2025-07-15 00:39:16 -0700

    GDS_MT backend support for LoopbackAgent
    Signed-off-by: Tomer Shmilovich <tshmilovich@nvidia.com>

--- cpp/include/tensorrt_llm/batch_manager/kvCacheManager.h
+++ cpp/include/tensorrt_llm/batch_manager/kvCacheManager.h
@@ -873,7 +873,7 @@ public:
         SizeType32 sinkBubbleLength, bool onboardBlocks, CacheType cacheType = CacheType::kSELF,
         std::optional<executor::RetentionPriority> secondaryOffloadMinPriority = std::nullopt,
         std::shared_ptr<KVCacheEventManager> eventManager = nullptr, bool enablePartialReuse = true,
-        bool copyOnPartialReuse = true);
+        bool copyOnPartialReuse = true, bool multiThreadReuse = false);
 
     BlockManager(BlockManager const&) = delete;
     BlockManager& operator=(BlockManager const&) = delete;
